3002994e0928ff97abd6e2edaa63596fd3617eb7
adnankhalid46 <adnankhalid46@gmail.com>
2020-06-10 01:31:56 +0500

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Hello from './Hello';
 
 function App({name, age}) {
-  return <div>
+  return <div> 
     Hello World from App.js updated {name} Age = {age-10}
     <br/>
     <Hello firstName={name}></Hello>
